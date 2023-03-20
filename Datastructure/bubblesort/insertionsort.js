a38044b5f03533cb5f2b185c3793a45dbbdb14e2
shashikant88093 <shashi94goswami@gmail.com>
2023-03-20 17:08:57 +0530

    added insertionsort

---
+++ Datastructure/bubblesort/insertionsort.js
@@ -0,0 +1,13 @@
+function insertioSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+console.log(insertioSort([2, 1, 9, 76, 4]));
