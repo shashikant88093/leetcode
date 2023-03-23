9f571ca6af789cd2a32c024bb180403b9b67dd73
shashikant88093 <shashi94goswami@gmail.com>
2023-03-24 01:13:16 +0530

    povit challenges

--- leetcode75/day2/povitindex.js
+++ leetcode75/day2/povitindex.js
@@ -5,9 +5,16 @@
 // Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
 // Right sum = nums[4] + nums[5] = 5 + 6 = 11
 
-function povit(arr){
-    let mid = arr[arr.length/2] 
-console.log(mid)
-
+function povit(nums){
+    let left = 0
+    let right = nums.reduce((a, b) => a + b);
+for (let i = 0; i < nums.length; ++i) {
+    right -= nums[i];
+    if (left == right) {
+        return i;
+    }
+    left += nums[i];
+}
+return -1; 
 }
 console.log(povit([1,7,3,6,5,6]))
