cad926db2f18653b28366db9970deff8b739dd46
shashikant88093 <shashi94goswami@gmail.com>
2023-04-10 17:50:54 +0530

    Signed-off-by: shashikant88093 <shashi94goswami@gmail.com>

--- Datastructure/singlylinkedlists/singlyLinkList.js
+++ Datastructure/singlylinkedlists/singlyLinkList.js
@@ -69,6 +69,56 @@ class SingleLinkList {
     this.length++
     return this
   }
+  get(index){
+   if(index < 0 || index >= this.length) return null;
+   let counter = 0
+   let current = this.head
+   while(counter != index){
+     current = current.next;
+     counter++
+   }
+   return current
+  }
+  set(index,val){
+  let foundNode = this.get(index)
+  if(foundNode){
+    foundNode.val = val
+    return true
+  }
+  return false
+  }
+  insert(index,val){
+  if(index < 0 || index > this.length) return false
+  if(index === this.length) return !!is.push(val)
+  if(index === 0) return !!is.unshift(val)
+  let newNode = new Node(val)
+  let prev = this.get(index - 1)
+  let temp = prev.next
+  prev.next = temp
+  this.length++
+  return true
+  
+  }
+  remove(index){
+  if(index <0 || index >= this.length) return undefined;
+  if(index === 0) return this.shift()
+  if(index === this.length - 1) return this.pop();
+  let previousNode = this.get(index -1)
+  let removed = previousNode.next = removed.next;
+  ltis.length--
+  return removed
+   
+  }
+  print(){
+  let arr = []
+  let current = this.head
+  while(current){
+   arr.push(current.val)
+   current = current.next
+  }
+  console.log(arr)
+  }
+  
 }
 
 let first = new SingleLinkList();
@@ -77,4 +127,4 @@ first.push("shashikant");
 first.push("kumar");
 
 
-console.log(first);
+
