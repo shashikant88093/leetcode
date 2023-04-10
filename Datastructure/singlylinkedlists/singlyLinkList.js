4739f1c59aaae53686a52b08a8bb081eddd9a14e
shashikant88093 <shashi94goswami@gmail.com>
2023-04-10 17:58:37 +0530

    reversed

--- Datastructure/singlylinkedlists/singlyLinkList.js
+++ Datastructure/singlylinkedlists/singlyLinkList.js
@@ -109,6 +109,22 @@ class SingleLinkList {
   return removed
    
   }
+  reverse(){
+    let node = this.head
+    this.head = this.tail
+    this.tail = node
+    let next 
+    let prev = null
+
+    for(let i=0;i<this.length;i++){
+        next = node.next
+        node.next = prev
+        prev = node
+        node = next
+
+    }
+    return this
+  }
   print(){
   let arr = []
   let current = this.head
